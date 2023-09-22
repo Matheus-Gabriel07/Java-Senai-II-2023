dc72c6a5a7167becd7ac2e455bbc67c5abdc5da4
Matheus Gabriel <math.gsilva07@gmail.com>
2023-09-22 16:16:43 -0300

    Summary

---
+++ Aula03/Exemplo02/testeBanco.java
@@ -0,0 +1,8 @@
+package Aula03.Exemplo02;
+
+public class testeBanco {
+    public static void main(String[] args) {
+        banco rafael = new banco(25, "Rafael");
+        rafael.informacoes();
+    }
+}

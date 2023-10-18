cd46df2a94eb7aa7d66283d0e9b2366cd41add05
Matheus Gabriel <math.gsilva07@gmail.com>
2023-10-18 09:28:42 -0300

    Update TesteFigura.java

--- Aula09/Exemplo01/TesteFigura.java
+++ Aula09/Exemplo01/TesteFigura.java
@@ -2,7 +2,14 @@ package Aula09.Exemplo01;
 
 public class TesteFigura {
     public static void main(String[] args) {
-        Circulo newCirculo = new Circulo(2);
-        System.out.println(newCirculo.calcularArea());
+        Figura newQuadrado = new Quadrado(2);
+        Figura newCirculo = new Circulo(2);
+        Figura newTriangulo = new Triangulo(3, 5);
+
+        //Polimorfismo
+        System.out.println("\tPOLIMORFISMO"
+            + "\nÁrea do quadrado: " + newQuadrado.calcularArea() + " cm²"
+            + "\nÁrea do circulo: " + newCirculo.calcularArea() + " cm²"
+            + "\nÁrea do triângulo: " + newTriangulo.calcularArea() + " cm²");       
     }
 }

6886d166c9111eff9a404e12cce12b709f5ec7ce
Matheus Gabriel <math.gsilva07@gmail.com>
2023-10-20 10:52:05 -0300

    Create Flash.java

---
+++ Aula10/Exemplo02/Flash.java
@@ -0,0 +1,27 @@
+package Aula10.Exemplo02;
+
+//Interface Super Herói
+
+interface SuperHero {
+    void runFast();
+    void savePeople();
+    void usePower();
+}
+
+class Flash implements SuperHero {
+
+    @Override
+    public void runFast() {
+        System.out.println("");
+    }
+
+    @Override
+    public void savePeople() {
+        System.out.println("");
+    }
+
+    @Override
+    public void usePower() {
+        System.out.println("");
+    }
+}
